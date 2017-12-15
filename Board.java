e1659d5c801eb3770f0c119c1a2856a38c6047df
Gabriel Felix <gabfelixop@gmail.com>
2017-12-15 11:44:05 -0300

    Bugs Fixed

--- Board.java
+++ Board.java
@@ -1,14 +1,17 @@
+
+package minesweeper;
+
 import java.util.Random;
 
 public class Board {
-    private int totalMines = 10;
-    private int boardWidth = 10;
-    private int boardHeight = 10;
-    private int[][] mineField;
-    private Random rand = new Random();
+    public static int totalMines = 10;
+    public static int horizontalTiles = 10;
+    public static int verticalTiles = 10;
+    public static int[][] mineField;
+    public static Random rand = new Random();
     
     //Creates the board
-    public void setBoard(){
+    public static void setBoard(){
         
         createBoard();
          
@@ -18,21 +21,22 @@ public class Board {
     }
     
     //Initialize the board
-    public void createBoard(){
-        mineField = new int[boardWidth][boardHeight];
+    public static void createBoard(){
+        mineField = new int[horizontalTiles][verticalTiles];
         
-         for (int i = 0; i < boardWidth; i++) {
-            for (int j = 0; j < boardHeight; j++) {
+        for (int i = 0; i < horizontalTiles; i++) {
+            for (int j = 0; j < verticalTiles; j++) {
                 mineField[i][j] = 0;
             }
         }
     }
     
     //Set the mines in random positions
-    public void setMines(){
+    public static void setMines(){
         for (int i = 0; i < totalMines; i++){
-            int xMine = rand.nextInt(boardWidth);
-            int yMine = rand.nextInt(boardHeight);
+            
+            int xMine = rand.nextInt(horizontalTiles);
+            int yMine = rand.nextInt(verticalTiles);
             
             if (mineField[xMine][yMine] != -1){
                 mineField[xMine][yMine] = -1;
@@ -43,25 +47,26 @@ public class Board {
     }
     
     //Set the numbers adjacent to the mines 
-    public void setNumbers(){
-        for (int i = 0; i < boardWidth; i++) {
-            for (int j = 0; j < boardHeight; j++) {
+    public static void setNumbers(){
+        
+        for (int i = 0; i < horizontalTiles; i++) {
+            for (int j = 0; j < verticalTiles; j++) {
+                
                 if(mineField[i][j] == -1){
                     
-                    for (int h = i - 1; h < i + 1; h++) {
-                        for (int v = j - 1; v < j + 1; v++) {
-                    
-                            if(mineField[h][v] != -1){
+                    for (int h = i - 1; h <= i + 1; h++) {
+                        for (int v = j - 1; v <= j + 1; v++) {  
+                            
+                            boolean inBoundsX = (h >= 0) && (h < horizontalTiles);
+                            boolean inBoundsY = (v >= 0) && (v < verticalTiles);
+                            
+                            if(inBoundsX && inBoundsY && mineField[h][v] != -1){
                                 mineField[h][v] += 1;
-                            }
+                            }          
                         }
                     }
-                }
+                }                          
             }
         }
-    }
-    
-    
-    
-    
+    }    
 }
